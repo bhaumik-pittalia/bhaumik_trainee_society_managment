cfe954b4652f6a0463999233a2c24c4f4766f659
bhaumik-pittalia <bhaumik-pittalia@users.noreply.github.com>
2020-01-06 11:48:41 +0530

    [IMP] cmt: added a new module

---
+++ society_managment/__manifest__.py
@@ -0,0 +1,12 @@
+{
+	'name' : 'Society Managment',
+	'version' : '1.1',
+	'summary' : 'Manage Society as your requrment',
+	'type' : 'application',
+	'data' : [
+		'security/ir.model.access.csv',
+		'views/society_managment.xml'
+	],
+
+	'application' : True
+}
